d02f9f900c731d39c792b7e4b8ac793d3a441288
shivangirastogi94 <shivangirastogi94@gmail.com>
2020-08-31 18:34:56 +0530

    if.py added

---
+++ if.py
@@ -0,0 +1,12 @@
+num1= 100
+num2= 10
+if num1>num2:
+    print("Num1 is greater than Num2")
+elif num2>num1:
+    print("Num2 is greater than Num1")
+else: 
+    print("Num2 is equal to Num1")
+
+
+
+
